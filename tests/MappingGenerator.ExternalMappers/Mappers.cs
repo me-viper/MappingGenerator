4bd1c001cbe60c5cf479c3ffec6df2ffe6eb956b
akurochka <andrii.kurochka@zorallabs.com>
2022-04-12 13:50:59 +0300

    #16. Add assembly with external mappers.

---
+++ tests/MappingGenerator.ExternalMappers/Mappers.cs
@@ -0,0 +1,25 @@
+﻿using Talk2Bits.MappingGenerator.Abstractions;
+
+namespace MappingGenerator.ExternalMappers
+{
+    public class ExternalSource
+    {
+        public string Text { get; set; } = default!;
+    }
+
+    public class ExternalDestination
+    {
+        public string Text { get; set; } = default!;
+
+        public string ExternalText { get; set; } = default!;
+    }
+
+    [MappingGenerator(typeof(ExternalSource), typeof(ExternalDestination))]
+    public partial class ExternalMapper
+    {
+        private string MapExternalText(ExternalSource source)
+        {
+            return $"External{source.Text}";
+        }
+    }
+}
